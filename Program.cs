097c0782026940566f370f49001de4d4a8d71bfd
Sulthon <sultona412@gmail.com>
2023-06-03 20:39:56 +0700

    update: tambah method buat hitung bilanganya dan fix bug

--- Program.cs
+++ Program.cs
@@ -7,22 +7,55 @@
         static void Main(string[] args)
         {
             Console.Clear();
+            int? kalkulator = KalkulatorTerbilang("lima dikali lima");
+            Console.WriteLine($"Hasil : {kalkulator}");
+        }
 
-            readInput("");
+        static int? KalkulatorTerbilang(string input)
+        {
+            if (input.Contains("dikali") || input.Contains("kali"))
+            {
+                string[] operan = { " dikali ", " kali " };
+                string[] inputInArray = input.Split(operan, System.StringSplitOptions.RemoveEmptyEntries);
+                return ConvertTerbilangKeNum(inputInArray[0]) * ConvertTerbilangKeNum(inputInArray[1]);
+            }
+            else if (input.Contains("ditambah") || input.Contains("tambah"))
+            {
+                string[] operan = { " ditambah ", " tambah " };
+                string[] inputInArray = input.Split(operan, System.StringSplitOptions.RemoveEmptyEntries);
+                return ConvertTerbilangKeNum(inputInArray[0]) + ConvertTerbilangKeNum(inputInArray[1]);
+            }
+            else if (input.Contains("dikurang") || input.Contains("kurang"))
+            {
+                string[] operan = { " dikurang ", " kurang " };
+                string[] inputInArray = input.Split(operan, System.StringSplitOptions.RemoveEmptyEntries);
+                return ConvertTerbilangKeNum(inputInArray[0]) - ConvertTerbilangKeNum(inputInArray[1]);
+            }
+            else if (input.Contains("dibagi") || input.Contains("bagi"))
+            {
+                string[] operan = { " dibagi ", " bagi " };
+                string[] inputInArray = input.Split(operan, System.StringSplitOptions.RemoveEmptyEntries);
+                return ConvertTerbilangKeNum(inputInArray[0]) / ConvertTerbilangKeNum(inputInArray[1]);
+            }
+            return 0;
         }
 
         // NOTE : Method membaca array terbilang dan mengubahnya menjadi array angka
-        static void readInput(string input)
+        static int? ConvertTerbilangKeNum(string input)
         {
-            string[] arr = input.Split(' ');
+            input.ToLower();
+
+            if (input.Length == 0) return null;
+
+            input = ubahTerbilangIstimewa(input);
+
+            List<string> arr = input.Split(' ').ToList();
 
             List<int> tampungan = new List<int>();
 
-            tampilArrayStr(arr);
-
             int temporary = 0;
 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Count; i++)
             {
 
                 if (i % 2 == 1)
@@ -49,53 +82,42 @@
                     }
                     else
                     {
-                        Console.WriteLine("Format salah.");
-                        break;
+                        return null;
                     }
                 }
 
-
-                for (int j = 0; j < _dataAng.Length; j++)
+                if (i % 2 == 0)
                 {
-                    if (arr[i] == _dataAng[j])
+                    int? checkValue = null;
+                    for (int j = 0; j < _dataAng.Length; j++)
                     {
-                        temporary = j;
+                        if (arr[i] == _dataAng[j])
+                        {
+                            checkValue = j;
+                        }
                     }
+                    if (checkValue != null) temporary = (int)checkValue;
+                    else return null;
                 }
-
             }
 
-            if (arr.Length % 2 == 1) tampungan.Add(temporary);
+            if (arr.Count % 2 == 1) tampungan.Add(temporary);
 
             int hasil = 0;
             foreach (int t in tampungan) hasil += t;
 
-            tampilArrayInt(tampungan.ToArray());
-
-            Console.WriteLine(hasil);
+            return hasil;
         }
 
         // NOTE : Method untuk menangani terbilang istimewa (seratus, seribu) 
         // Cth  : [seratus] -> [satu][ratus]
-        static void ubahTerbilangIstimewa(string[] arr)
+        static string ubahTerbilangIstimewa(string input)
         {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                string[] tampungan = new string[2];
-                if (arr[i] == "seribu")
-                {
-                    tampungan[0] = "satu";
-                    tampungan[1] = "ribu";
-                    arr.Concat(tampungan);
-                }
-                else if (arr[i] == "seratus")
-                {
-                    tampungan[0] = "satu";
-                    tampungan[1] = "ratus";
-                }
-            }
-
-            tampilArrayStr(arr);
+            if (input.Contains("seribu"))
+                return input.Replace("seribu", "satu ribu");
+            else if (input == "seratus")
+                return input.Replace("seratus", "satu ratus");
+            return input;
         }
 
         // NOTE : Method menampilkan array tipe data int
